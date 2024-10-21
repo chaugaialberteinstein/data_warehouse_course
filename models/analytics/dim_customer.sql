f7c26a7265882c009a98b78683f0aee857a7b428
chaugaialberteinstein <hathanhnga216@gmail.com>
2024-10-21 14:09:27 +0700

    0107c dim customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -8,6 +8,8 @@ WITH dim_customer__source AS (
 	SELECT
 		customer_id AS  customer_key,
 		customer_name
+    , customer_category_id AS customer_category_key
+    , buying_group_id AS buying_group_key
 	FROM dim_customer__source
 )
 
@@ -15,10 +17,18 @@ WITH dim_customer__source AS (
 	SELECT
 		CAST(customer_key AS INTEGER) AS customer_key
 		, CAST(customer_name AS STRING) AS customer_name	
+    , CAST( customer_category_key AS INTEGER) AS customer_category_key
+    , CAST( buying_group_key AS INTEGER) AS buying_group_key
 	FROM dim_customer__rename_column	
 )
 
 SELECT
-	customer_key
-	, customer_name
-FROM dim_customer__cast_type
+	dim_customer.customer_key
+	,dim_customer.customer_name
+  ,dim_customer.customer_category_key
+  ,dim_customer__category.customer_category_name
+  ,dim_customer.buying_group_key
+FROM dim_customer__cast_type AS dim_customer
+LEFT JOIN {{ref('stg_dim_customer_category')}} AS dim_customer__category
+ON  dim_customer.customer_category_key = dim_customer__category.customer_category_key
+
